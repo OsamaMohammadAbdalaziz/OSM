9cf7cd9e0908b05c4026105125bee72d12d6acea
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-10 19:30:05 +0200

    Documentation for project

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+dotenv.config();
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(process || port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
