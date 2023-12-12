d5f13abeefc916acf1dec5d0c68e8cc2fa3fd749
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-12 20:18:08 +0200

    To specify timestamps and to specify the model

--- models/Cart.js
+++ models/Cart.js
@@ -1,2 +1,3 @@
 const mongoose = require("mongoose");
-const CartShecma = new mongoose.Shcema({});
+const CartShecma = new mongoose.Shcema({}, { timestamps: true });
+module.exports = mongoose.model("Cart", CartShecma);
