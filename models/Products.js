273eb6fe217c168878ce1a462ca735c5aacf501c
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-12 20:11:56 +0200

    To specify timestamps and to specify the model

--- models/Products.js
+++ models/Products.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose");
-const ProductShecma = new mongoose.Shcema({
-  Name: { type: String, required: true },
-  Title: { type: String, required: true },
-  Category: { type: String, required: true },
-  Imageurl: { type: [String], required: true },
-  OldPrice: { type: String, required: true },
-  Sizes: {
-    type: [
-      {
-        Size: { type: String, required: true },
-        IsSelected: { type: Boolean, required: false, default: false },
-      },
-    ],
+const ProductShecma = new mongoose.Shcema(
+  {
+    Name: { type: String, required: true },
+    Title: { type: String, required: true },
+    Category: { type: String, required: true },
+    Imageurl: { type: [String], required: true },
+    OldPrice: { type: String, required: true },
+    Sizes: {
+      type: [
+        {
+          Size: { type: String, required: true },
+          IsSelected: { type: Boolean, required: false, default: false },
+        },
+      ],
+    },
+    Price: { type: String, required: true },
+    Description: { type: String, required: true },
   },
-  Price: { type: String, required: true },
-  Description: { type: String, required: true },
-});
+  { timestamps: true }
+);
+module.exports = mongoose.model("Product", ProductShecma);
