af1ecc060440d34a23aa2560092dfd496ff27494
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-12 20:21:31 +0200

    To specify timestamps and to specify the model

--- models/Users.js
+++ models/Users.js
@@ -1,2 +1,3 @@
 const mongoose = require("mongoose");
-const UsersShecma = new mongoose.Shcema({});
+const UsersShecma = new mongoose.Shcema({}, { timestamps: true });
+module.exports = mongoose.model("Users", UsersShecma);
