bcdddf1487eb1a460eb09d7b04e1c5ad979d77ff
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-12 20:08:46 +0200

    Create a table database for Products

--- models/Products.js
+++ models/Products.js
@@ -1,2 +1,18 @@
 const mongoose = require("mongoose");
-const ProductShecma = new mongoose.Shcema({});
+const ProductShecma = new mongoose.Shcema({
+  Name: { type: String, required: true },
+  Title: { type: String, required: true },
+  Category: { type: String, required: true },
+  Imageurl: { type: [String], required: true },
+  OldPrice: { type: String, required: true },
+  Sizes: {
+    type: [
+      {
+        Size: { type: String, required: true },
+        IsSelected: { type: Boolean, required: false, default: false },
+      },
+    ],
+  },
+  Price: { type: String, required: true },
+  Description: { type: String, required: true },
+});
