bd8bf67fa2f9959b4e33d9488d93e84819cb755e
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-12 21:19:04 +0200

    Create a table database for Users. The Emails for users is unique.

--- models/Users.js
+++ models/Users.js
@@ -1,3 +1,11 @@
 const mongoose = require("mongoose");
-const UsersSchema = new mongoose.Schema({}, { timestamps: true });
+const UsersSchema = new mongoose.Schema(
+  {
+    UserName: { type: String, required: true },
+    Email: { type: String, required: true, unique: true },
+    Password: { type: String, required: true },
+    Location: { type: String, default: "Dubai UAE" },
+  },
+  { timestamps: true }
+);
 module.exports = mongoose.model("Users", UsersSchema);
