111a75026ffdf41df5786401a6c0e2f38e6722eb
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-12 20:29:38 +0200

    Create a table database for Cart

--- models/Cart.js
+++ models/Cart.js
@@ -1,3 +1,17 @@
 const mongoose = require("mongoose");
-const CartShecma = new mongoose.Shcema({}, { timestamps: true });
+const CartShecma = new mongoose.Shcema(
+  {
+    UserId: { type: String, required: true },
+    Products: [
+      {
+        CartItem: {
+          type: mongoose.Schema.Types.ObjectId,
+          Reference: "Product",
+        },
+        Quantity: { type: Number, default: 1 },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 module.exports = mongoose.model("Cart", CartShecma);
