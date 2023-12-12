e84cc361e1156b0911e4f5cc87b259be12713187
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-12 19:16:46 +0200

    Connecting the server to mongo db

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const port = 3000;
 
 dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
 app.listen(process.env.PORT || port, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
 );
