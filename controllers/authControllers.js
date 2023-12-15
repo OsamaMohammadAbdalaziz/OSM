6c1d53fb7fc6fce833a946abb783f71f8a7e738f
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-16 00:09:21 +0200

    Decrypting the Passwords with crypto-js & creating JWT token

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,6 @@
 const Users = require("../models/Users");
 
-const cryptojs = require("crypto-js");
+const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
 module.exports = {
@@ -8,7 +8,7 @@ module.exports = {
     const NewUser = new Users({
       UserName: req.body.UserName,
       Email: req.body.Email,
-      Password: cryptojs.AES.encrypt(
+      Password: CryptoJS.AES.encrypt(
         req.body.Password,
         process.env.SECRET
       ).toString(),
@@ -21,5 +21,33 @@ module.exports = {
       res.status(500).json({ Message: error });
     }
   },
-  LoginUser: async (req, res) => {},
+  LoginUser: async (req, res) => {
+    try {
+      const User = await Users.find({ Email: req.body.Email });
+      !User && res.status(401).json({ Message: "Could not find the User" });
+
+      const DecryptedPassword = CryptoJS.AES.decrypt(
+        User.Password,
+        process.env.SECRET
+      );
+      const ThePassword = DecryptedPassword.toString(CryptoJS.enc.Utf8);
+      ThePassword !== req.body.Password &&
+        res.status(401).json({ Message: "Wrong Password" });
+
+      const UserToken = jwt.sign(
+        {
+          Id: User._Id,
+        },
+        process.env.JWT_SEC,
+        { expiresIn: "21d" }
+      );
+      const { Password, __v, createdAt, ...others } = User._doc;
+
+      res.status(200).json({ ...others, token: UserToken });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ Message: "Failed to login check your credentials" });
+    }
+  },
 };
