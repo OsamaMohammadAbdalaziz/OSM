d96d735e8595a1304e97fb1dc375fb1e1e6b432e
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-16 21:16:19 +0200

    Verifying the jwt token

--- middleware/VerifyToken.js
+++ middleware/VerifyToken.js
@@ -1,4 +1,19 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/Users");
 
-const VerifyToken = (req, res, next) => {};
+const VerifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, async (err, User) => {
+      if (err) res.status(403).json("Invalid token");
+      req.User = User;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated");
+  }
+};
+
+module.exports = { VerifyToken };
