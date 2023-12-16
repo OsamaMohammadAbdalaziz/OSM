39fb368f48323e9f12782cc0fb31f1c7c580b92c
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-16 23:01:23 +0200

    Creating CartControllers function NO.1 AddCart

---
+++ controllers/CartControllers.js
@@ -0,0 +1,38 @@
+const Products = require("../models/Products");
+const Cart = require("../models/Cart");
+const Cart = require("../models/Cart");
+
+module.exports = {
+  AddCart: async (req, res) => {
+    const UserId = req.User.Id;
+    const { CartItem, Quantity } = req.body;
+
+    try {
+      const Cart = await Cart.findOne({ UserId });
+
+      if (Cart) {
+        const ExistingProduct = Cart.Products.find(
+          (Products) => Products.CartItem.toString() === CartItem
+        );
+
+        if (ExistingProduct) {
+          ExistingProduct.Quantity += 1;
+        } else {
+          Cart.Products.push({ CartItem, Quantity: 1 });
+        }
+        await Cart.save();
+        res.status(200).json("Product added to cart");
+      } else {
+        const newCart = new Cart({
+          UserId,
+          Products: [{ CartItem, Quantity: 1 }],
+        });
+
+        await newCart.save();
+        res.status(200).json("Product added to cart");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+};
