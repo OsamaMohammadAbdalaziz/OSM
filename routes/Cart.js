c5f4dbe0a599f4668cc7535171266d40ac20d03b
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-16 23:16:22 +0200

    Creating CartRoutes

---
+++ routes/Cart.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const CartControllers = require("../controllers/CartControllers");
+const { VerifyToken } = require("../middleware/VerifyToken");
+
+router.get("/find/", VerifyToken, CartControllers.getCart);
+router.add("/", VerifyToken, CartControllers.AddCart);
+router.delete("/:CartItem", VerifyToken, CartControllers.deleteCartItem);
+
+module.exports = router;
