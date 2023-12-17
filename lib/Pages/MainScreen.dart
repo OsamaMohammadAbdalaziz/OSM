ef9bfd4d5b0a4dfa5c68c5432ffbd60295a8b201
OsamaMohammadAbdalaziz <arabradui170@gmail.com>
2023-12-18 00:34:12 +0200

    Create a MainScreen Page & Edit the main page

---
+++ lib/Pages/MainScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text(
+            "MainScreen",
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
